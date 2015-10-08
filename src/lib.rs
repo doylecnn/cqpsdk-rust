821f9e6bc748ddf1091ea7e2ebb5de9855e0771e
evshiron <evshiron@gmail.com>
2015-10-08 16:32:51 +0800

    Add convenient macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
 pub mod cqpapi;
+
+#[macro_export]
+macro_rules! gbk {
+
+	( $x: expr ) => (CString::new(GBK.encode($x, EncoderTrap::Ignore).unwrap()).unwrap().as_ptr());
+	
+}
+
+#[macro_export]
+macro_rules! utf8 {
+	
+	( $x: expr ) => (&GBK.decode(CStr::from_ptr($x).to_bytes(), DecoderTrap::Ignore).unwrap()[..]);
+	
+}
+
